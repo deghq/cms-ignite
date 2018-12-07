ff4e6cc64f78cbc52dd36ff223b92e62c25d7680
Ian <ian.escarro@gmail.com>
2018-12-08 00:01:35 +0800

    Separate contact view

--- application/views/themes/spectrum/layout.php
+++ application/views/themes/spectrum/layout.php
@@ -86,29 +86,10 @@
             </li>
             <?php foreach ($links as $link): ?>
             <li class="page-scroll">
-              <?php echo anchor('home/page/' . $link->page_id, $link->name); ?>
+              <?php $href = $link->is_page_reference ? 'home/page/' . $link->page_id : $link->reference; ?>
+              <?php echo anchor($href, $link->name); ?>
             </li>
             <?php endforeach; ?>
-<!--
-            <li class="page-scroll">
-              <a href="#about">About</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#features">Features</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#work">Work</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#blog">Blog</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#pricing">Pricing</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#contact">Contact</a>
-            </li>
--->
           </ul>
         </div>
         <!-- /.navbar-collapse -->
@@ -788,50 +769,7 @@
       </div>
     </section>
 
-    <section id="contact" class="contact-1">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6" data-scrollreveal="enter left over 1.5s">
-            <h2>Contact Us</h2>
-            <p>Spectrum comes loaded with a working contact form! Just follow the instructions in the documentation to get your form working. All you need to do is change an email address, no code writing required!</p>
-            <p>The contact form to the right is a demo form and will not send email to Start Bootstrap. If you have a question about the theme, please contact us through our WrapBootstrap profile.</p>
-          </div>
-          <div class="col-lg-5 col-lg-offset-1" data-scrollreveal="enter right over 1.5s">
-            <br>
-            <form name="sentMessage" id="contactForm" novalidate>
-              <div class="row control-group">
-                <div class="form-group col-xs-12 floating-label-form-group controls">
-                  <label>Name</label>
-                  <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                  <p class="help-block text-danger"></p>
-                </div>
-              </div>
-              <div class="row control-group">
-                <div class="form-group col-xs-12 floating-label-form-group controls">
-                  <label>Email Address</label>
-                  <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                  <p class="help-block text-danger"></p>
-                </div>
-              </div>
-              <div class="row control-group">
-                <div class="form-group col-xs-12 floating-label-form-group controls">
-                  <label>Message</label>
-                  <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                  <p class="help-block text-danger"></p>
-                </div>
-              </div>
-              <br>
-              <div id="success"></div>
-              <div class="row">
-                <div class="form-group col-xs-12">
-                  <button type="submit" class="btn btn-lg btn-primary btn-square btn-raised">Send</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
+    <?php load_view('contact'); ?>
 
     <footer class="footer-1">
       <div class="upper">
