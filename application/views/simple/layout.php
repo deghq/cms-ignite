e1b99971e13d60e7f0b3213f7be04ead42458077
Ian <ian.escarro@gmail.com>
2018-12-07 21:47:41 +0800

    Added links utility

--- application/views/simple/layout.php
+++
@@ -1,9 +0,0 @@
-[
-  <?php echo anchor('dashboard', 'Dashboard'); ?>
-  | <?php echo anchor('posts', 'Blog posts'); ?>
-  | <?php echo anchor('pages', 'Pages'); ?>
-  | <?php echo anchor('users', 'Users'); ?>
-  | <?php echo anchor('logout', 'Log out'); ?>  
-]
-
-<?php echo $content; ?>
